b893e32747a8557953db852ac87d1015d421744c
Daniel Golden <dgolden@newrelic.com>
2020-01-08 14:21:17 -0500

    refactor: Move empty state into a component
    We found that it was unclear that the `.empty-state`
    stack component was supposed to be replaced rather
    than used as a container for user content. So in this
    commit we move the empty state into it's own
    component to make it more difficult for users to get this
    wrong.
    
    newrelic/open-source-office#26

--- nerdlets/my-new-nerdlet/index.js
+++ nerdlets/my-new-nerdlet/index.js
@@ -9,10 +9,11 @@ import {
   TextField,
   Button
 } from 'nr1';
+import EmptyState from '../components/EmptyState';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
-export default class NerdpackLayoutTBone extends React.Component {
+export default class NerdpackLayout4x2Grid extends React.Component {
   render() {
     return (
       <>
@@ -63,168 +64,60 @@ export default class NerdpackLayoutTBone extends React.Component {
             Grid.SPACING_TYPE.LARGE
           ]}
         >
-          {/*
-            Note: This sidebar does _not_ have to be a list of links/navigation.
-            It can just as easily contain content. This is just an example of how it
-            may be used.
-          */}
           <GridItem className="primary-content-container" columnSpan={3}>
             <main className="primary-content full-height">
-              <Stack
-                className="empty-state"
-                fullWidth
-                fullHeight
-                verticalType={Stack.VERTICAL_TYPE.CENTER}
-                horizontalType={Stack.HORIZONTAL_TYPE.CENTER}
-                directionType={Stack.DIRECTION_TYPE.VERTICAL}
-                gapType={Stack.GAP_TYPE.NONE}
-              >
-                <StackItem>
-                  <h4 className="empty-state-header">Section 1</h4>
-                </StackItem>
-                <StackItem>
-                  <p className="empty-state-description">
-                    Wondering <strong>how to use this layout</strong>? check out
-                    our tutorial on{' '}
-                    <a
-                      href="https://discuss.newrelic.com/t/how-to-populate-your-nerdpack-layout-template-with-data/90955"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      how to populate your Nerdpack layout template with data.
-                    </a>{' '}
-                    Have suggestions, concerns, or ideas for how this template
-                    could be better? Feel free to{' '}
-                    <a
-                      href="https://github.com/newrelic/nr1-nerdpack-layout-4x2-grid"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      submit an issue or PR
-                    </a>{' '}
-                    :) .
-                  </p>
-                </StackItem>
-              </Stack>
+              {/* ⌄⌄ REPLACE THE LINE _BELOW_ WITH YOUR CONTENT ⌄⌄ */}
+              <EmptyState heading="Section 1" />
+              {/* ^^ REPLACE THE LINE _ABOVE_ WITH YOUR CONTENT ^^ */}
             </main>
           </GridItem>
           <GridItem className="primary-content-container" columnSpan={3}>
             <main className="primary-content full-height">
-              <Stack
-                className="empty-state"
-                fullWidth
-                fullHeight
-                verticalType={Stack.VERTICAL_TYPE.CENTER}
-                horizontalType={Stack.HORIZONTAL_TYPE.CENTER}
-                directionType={Stack.DIRECTION_TYPE.VERTICAL}
-                gapType={Stack.GAP_TYPE.NONE}
-              >
-                <StackItem>
-                  <h4 className="empty-state-header">Section 2</h4>
-                </StackItem>
-              </Stack>
+              {/* ⌄⌄ REPLACE THE LINE _BELOW_ WITH YOUR CONTENT ⌄⌄ */}
+              <EmptyState heading="Section 2" description=" " />
+              {/* ^^ REPLACE THE LINE _ABOVE_ WITH YOUR CONTENT ^^ */}
             </main>
           </GridItem>
           <GridItem className="primary-content-container" columnSpan={3}>
             <main className="primary-content full-height">
-              <Stack
-                className="empty-state"
-                fullWidth
-                fullHeight
-                verticalType={Stack.VERTICAL_TYPE.CENTER}
-                horizontalType={Stack.HORIZONTAL_TYPE.CENTER}
-                directionType={Stack.DIRECTION_TYPE.VERTICAL}
-                gapType={Stack.GAP_TYPE.NONE}
-              >
-                <StackItem>
-                  <h4 className="empty-state-header">Section 3</h4>
-                </StackItem>
-              </Stack>
+              {/* ⌄⌄ REPLACE THE LINE _BELOW_ WITH YOUR CONTENT ⌄⌄ */}
+              <EmptyState heading="Section 3" description=" " />
+              {/* ^^ REPLACE THE LINE _ABOVE_ WITH YOUR CONTENT ^^ */}
             </main>
           </GridItem>
           <GridItem className="primary-content-container" columnSpan={3}>
             <main className="primary-content full-height">
-              <Stack
-                className="empty-state"
-                fullWidth
-                fullHeight
-                verticalType={Stack.VERTICAL_TYPE.CENTER}
-                horizontalType={Stack.HORIZONTAL_TYPE.CENTER}
-                directionType={Stack.DIRECTION_TYPE.VERTICAL}
-                gapType={Stack.GAP_TYPE.NONE}
-              >
-                <StackItem>
-                  <h4 className="empty-state-header">Section 4</h4>
-                </StackItem>
-              </Stack>
+              {/* ⌄⌄ REPLACE THE LINE _BELOW_ WITH YOUR CONTENT ⌄⌄ */}
+              <EmptyState heading="Section 4" description=" " />
+              {/* ^^ REPLACE THE LINE _ABOVE_ WITH YOUR CONTENT ^^ */}
             </main>
           </GridItem>
           <GridItem className="primary-content-container" columnSpan={3}>
             <main className="primary-content full-height">
-              <Stack
-                className="empty-state"
-                fullWidth
-                fullHeight
-                verticalType={Stack.VERTICAL_TYPE.CENTER}
-                horizontalType={Stack.HORIZONTAL_TYPE.CENTER}
-                directionType={Stack.DIRECTION_TYPE.VERTICAL}
-                gapType={Stack.GAP_TYPE.NONE}
-              >
-                <StackItem>
-                  <h4 className="empty-state-header">Section 5</h4>
-                </StackItem>
-              </Stack>
+              {/* ⌄⌄ REPLACE THE LINE _BELOW_ WITH YOUR CONTENT ⌄⌄ */}
+              <EmptyState heading="Section 5" description=" " />
+              {/* ^^ REPLACE THE LINE _ABOVE_ WITH YOUR CONTENT ^^ */}
             </main>
           </GridItem>
           <GridItem className="primary-content-container" columnSpan={3}>
             <main className="primary-content full-height">
-              <Stack
-                className="empty-state"
-                fullWidth
-                fullHeight
-                verticalType={Stack.VERTICAL_TYPE.CENTER}
-                horizontalType={Stack.HORIZONTAL_TYPE.CENTER}
-                directionType={Stack.DIRECTION_TYPE.VERTICAL}
-                gapType={Stack.GAP_TYPE.NONE}
-              >
-                <StackItem>
-                  <h4 className="empty-state-header">Section 6</h4>
-                </StackItem>
-              </Stack>
+              {/* ⌄⌄ REPLACE THE LINE _BELOW_ WITH YOUR CONTENT ⌄⌄ */}
+              <EmptyState heading="Section 6" description=" " />
+              {/* ^^ REPLACE THE LINE _ABOVE_ WITH YOUR CONTENT ^^ */}
             </main>
           </GridItem>
           <GridItem className="primary-content-container" columnSpan={3}>
             <main className="primary-content full-height">
-              <Stack
-                className="empty-state"
-                fullWidth
-                fullHeight
-                verticalType={Stack.VERTICAL_TYPE.CENTER}
-                horizontalType={Stack.HORIZONTAL_TYPE.CENTER}
-                directionType={Stack.DIRECTION_TYPE.VERTICAL}
-                gapType={Stack.GAP_TYPE.NONE}
-              >
-                <StackItem>
-                  <h4 className="empty-state-header">Section 7</h4>
-                </StackItem>
-              </Stack>
+              {/* ⌄⌄ REPLACE THE LINE _BELOW_ WITH YOUR CONTENT ⌄⌄ */}
+              <EmptyState heading="Section 7" description=" " />
+              {/* ^^ REPLACE THE LINE _ABOVE_ WITH YOUR CONTENT ^^ */}
             </main>
           </GridItem>
           <GridItem className="primary-content-container" columnSpan={3}>
             <main className="primary-content full-height">
-              <Stack
-                className="empty-state"
-                fullWidth
-                fullHeight
-                verticalType={Stack.VERTICAL_TYPE.CENTER}
-                horizontalType={Stack.HORIZONTAL_TYPE.CENTER}
-                directionType={Stack.DIRECTION_TYPE.VERTICAL}
-                gapType={Stack.GAP_TYPE.NONE}
-              >
-                <StackItem>
-                  <h4 className="empty-state-header">Section 8</h4>
-                </StackItem>
-              </Stack>
+              {/* ⌄⌄ REPLACE THE LINE _BELOW_ WITH YOUR CONTENT ⌄⌄ */}
+              <EmptyState heading="Section 8" description=" " />
+              {/* ^^ REPLACE THE LINE _ABOVE_ WITH YOUR CONTENT ^^ */}
             </main>
           </GridItem>
         </Grid>
